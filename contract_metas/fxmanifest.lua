a4726f7d92daa232ebb52c6ee9c58cfc4d71cd9f
TheGoldenRetriever19 <88576905+TheGoldenRetriever19@users.noreply.github.com>
2022-01-03 19:34:03 +0000

    Add files via upload

--- contract_metas/fxmanifest.lua
+++ contract_metas/fxmanifest.lua
@@ -10,3 +10,4 @@ files {
 data_file 'HANDLING_FILE' 'data/handling.meta'
 data_file 'CARCOLS_FILE' 'data/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
